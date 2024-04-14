0132507c7583660ce6566e386bd8bf4b5df4280a
a-nester <a_nester@ukr.net>
2024-04-14 19:23:32 +0300

    +

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ async function handleClick(event) {
     } catch {
         iziToast.error({
             position: "topRight",
-            message: "Sorry, there are no images matching your search query. Please try again!",
+            message: "Something went wrong. Please try again!",
         })
     } finally {
             searchForm.elements[0].value = "";
